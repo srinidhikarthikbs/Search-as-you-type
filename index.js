4b4919128d4a8ee590e65ea05fb0b30e422590ed
Srinidhi Karthik B S <srinidhikarthikbs@gmail.com>
2018-01-02 17:36:33 -0800

    minor changes

--- index.js
+++ index.js
@@ -7,39 +7,19 @@ var bodyParser = require('body-parser');
 var cors = require('cors');
 var request = require('request');
 var htmlToText = require('html-to-text');
-// instantiate a new dictionary
 var dict = spell();
-// load text into dictionary so we can train the dictionary to know
-// which words exists and which ones are more frequent than others
-
-var dic_loaded = false;//make false for spellcheck to work
-
+var dic_loaded = false;
 
 app.get('/', (req, res) => {
-	// if(!dic_loaded){
-	// 	fs.readFile('big.txt', 'utf8', function (err,data) {
-	// 		dict.load(data);
-	// 		dic_loaded = true;
-	// 		console.log('dictionary loaded');
-
-	// 		console.log(dict.suggest('politicx'));
-	// 		res.render('index.ejs');
-	// 	});
-	// }
-	// else{
-	// 	res.render('index.ejs');
-	// }
 	res.render('index.ejs');
 });
 
 app.get('/getcsv', (req, res) => {
-	fs.readFile('mapNBCNewsDataFile.csv', 'utf8', function (err,data) {
-		if(err){
+	fs.readFile('mapNBCNewsDataFile.csv', 'utf8', function (err, data) {
+		if (err) {
 			console.log('error reading csv file');
 			res.send("error,error");
-		}
-		else {
-			//console.log(data);
+		} else {
 			res.send(data);
 		}
 	});
@@ -50,52 +30,40 @@ app.get('/getcorrected/:word', (req, res) => {
 });
 
 app.get('/getsnippet', (req, res) => {
-	console.log(req.query.url)
-	// request(req.query.url, function (error, response, body) {
-	// 	if(response.statusCode!=200) res.send("Error fetching content");
-	// 	var text = htmlToText.fromString(body, {
-	// 		wordwrap: false
-	// 	});
-	// 	res.send(text);
-	// });
-
-	htmlToText.fromFile(path.join(__dirname, 'tikaoutput/'+req.query.url+".txt"), {
+	htmlToText.fromFile(path.join(__dirname, 'tikaoutput/' + req.query.url + ".txt"), {
 		tables: true
 	}, (err, text) => {
 		if (err) return console.error(err);
-		//console.log(text);
 		res.send(text);
 	});
 });
 
 app.post('/getcorrectedsentence', (req, res) => {
-	//res.send(JSON.stringify(dict.suggest(word)));
 	var sentence = req.query.sentence.trim().split(/[ ]+/);
-	console.log(sentence);
-	if(sentence[0]==="*:*"){
+	if (sentence[0] === "*:*") {
 		res.send("*:*");
-	}
-	else {
+	} else {
 		var corrected_words = [];
-		for(var i=0;i<sentence.length;i++){
+		for (var i = 0; i < sentence.length; i++) {
 			var dict_suggest = dict.suggest(sentence[i]);
-			console.log(dict_suggest);
-			if(dict_suggest.length>0)
+			if (dict_suggest.length > 0)
 				corrected_words.push(dict_suggest);
-			else 
-				corrected_words.push([{ word: sentence[i], score: 1 }]);
+			else
+				corrected_words.push([{
+					word: sentence[i],
+					score: 1
+				}]);
 		}
 		var corrected_sentence = "";
-		for(var i=0;i<corrected_words.length;i++){
-			corrected_sentence+=corrected_words[i][0].word+" ";
+		for (var i = 0; i < corrected_words.length; i++) {
+			corrected_sentence += corrected_words[i][0].word + " ";
 		}
-		console.log(corrected_sentence)
 		res.send(corrected_sentence.trim());
 	}
 });
 
 //CORS middleware
-var allowCrossDomain = function(req, res, next) {
+var allowCrossDomain = function (req, res, next) {
 	res.header('Access-Control-Allow-Origin', '*');
 	next();
 }
@@ -108,21 +76,17 @@ app.use(bodyParser.urlencoded({
 
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname));
-//app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs'); // set up ejs for templating
 app.use(cors());
-// Start the server
+
 const PORT = process.env.PORT || 8090;
 
-fs.readFile('big.txt', 'utf8', function (err,data) {
+fs.readFile('big.txt', 'utf8', function (err, data) {
 	dict.load(data);
 	dic_loaded = true;
-	console.log('dictionary loaded');
 
-	console.log(dict.suggest('politicx'));
 	app.listen(PORT, () => {
 		console.log(`App listening on port ${PORT}`);
 		console.log('Press Ctrl+C to quit.');
 	});
 });
-// [END app]
--- script.js
+++ script.js
@@ -1,195 +1,161 @@
 var irhw5 = angular.module('irhw5', ['ngAnimate', 'ngSanitize', 'ui.bootstrap']);
-irhw5.controller('spellauto', ['$scope', '$window', '$document', '$http', function(scope, window, document, http){
-    scope.stopwords = ["a", "about", "above", "above", "across", "after", "afterwards", "again", "against", "all", "almost", "alone",   "along", "already", "also","although","always","am","among", "amongst", "amoungst", "amount",  "an", "and", "another", "any","anyhow","anyone","anything","anyway", "anywhere", "are", "around", "as",  "at", "back","be","became", "because","become","becomes", "becoming", "been", "before", "beforehand", "behind", "being", "below", "beside", "besides", "between", "beyond", "bill", "both", "bottom","but", "by", "call", "can", "cannot", "cant", "co", "con", "could", "couldnt", "cry", "de", "describe", "detail", "do", "done", "down", "due", "during", "each", "eg", "eight", "either", "eleven","else", "elsewhere", "empty", "enough", "etc", "even", "ever", "every", "everyone", "everything", "everywhere", "except", "few", "fifteen", "fify", "fill", "find", "fire", "first", "five", "for", "former", "formerly", "forty", "found", "four", "from", "front", "full", "further", "get", "give", "go", "had", "has", "hasnt", "have", "he", "hence", "her", "here", "hereafter", "hereby", "herein", "hereupon", "hers", "herself", "him", "himself", "his", "how", "however", "hundred", "ie", "if", "in", "inc", "indeed", "inteValuest", "into", "is", "it", "its", "itself", "keep", "last", "latter", "latterly", "least", "less", "ltd", "made", "many", "may", "me", "meanwhile", "might", "mill", "mine", "more", "moreover", "most", "mostly", "move", "much", "must", "my", "myself", "name", "namely", "neither", "never", "nevertheless", "next", "nine", "no", "nobody", "none", "noone", "nor", "not", "nothing", "now", "nowhere", "of", "off", "often", "on", "once", "one", "only", "onto", "or", "other", "others", "otherwise", "our", "ours", "ourselves", "out", "over", "own","part", "per", "perhaps", "please", "put", "rather", "re", "same", "see", "seem", "seemed", "seeming", "seems", "serious", "several", "she", "should", "show", "side", "since", "sincere", "six", "sixty", "so", "some", "somehow", "someone", "something", "sometime", "sometimes", "somewhere", "still", "such", "system", "take", "ten", "than", "that", "the", "their", "them", "themselves", "then", "thence", "there", "thereafter", "thereby", "therefore", "therein", "thereupon", "these", "they", "thickv", "thin", "third", "this", "those", "though", "three", "through", "throughout", "thru", "thus", "to", "together", "too", "top", "toward", "towards", "twelve", "twenty", "two", "un", "under", "until", "up", "upon", "us", "very", "via", "was", "we", "well", "were", "what", "whatever", "when", "whence", "whenever", "where", "whereafter", "whereas", "whereby", "wherein", "whereupon", "wherever", "whether", "which", "while", "whither", "who", "whoever", "whole", "whom", "whose", "why", "will", "with", "within", "without", "would", "yet", "you", "your", "yours", "yourself", "yourselves", "the"];
-    scope.index_method = "Lucene";
-    scope.q = "";
-    scope.corrected_query = "";
-    scope.display_corrected_query = false;
-    scope.filtered_suggestions = [];
 
-    scope.file_csv = [];
+irhw5.controller('spellauto', ['$scope', '$window', '$document', '$http', function (scope, window, document, http) {
+	scope.stopwords = ["a", "about", "above", "above", "across", "after", "afterwards", "again", "against", "all", "almost", "alone", "along", "already", "also", "although", "always", "am", "among", "amongst", "amoungst", "amount", "an", "and", "another", "any", "anyhow", "anyone", "anything", "anyway", "anywhere", "are", "around", "as", "at", "back", "be", "became", "because", "become", "becomes", "becoming", "been", "before", "beforehand", "behind", "being", "below", "beside", "besides", "between", "beyond", "bill", "both", "bottom", "but", "by", "call", "can", "cannot", "cant", "co", "con", "could", "couldnt", "cry", "de", "describe", "detail", "do", "done", "down", "due", "during", "each", "eg", "eight", "either", "eleven", "else", "elsewhere", "empty", "enough", "etc", "even", "ever", "every", "everyone", "everything", "everywhere", "except", "few", "fifteen", "fify", "fill", "find", "fire", "first", "five", "for", "former", "formerly", "forty", "found", "four", "from", "front", "full", "further", "get", "give", "go", "had", "has", "hasnt", "have", "he", "hence", "her", "here", "hereafter", "hereby", "herein", "hereupon", "hers", "herself", "him", "himself", "his", "how", "however", "hundred", "ie", "if", "in", "inc", "indeed", "inteValuest", "into", "is", "it", "its", "itself", "keep", "last", "latter", "latterly", "least", "less", "ltd", "made", "many", "may", "me", "meanwhile", "might", "mill", "mine", "more", "moreover", "most", "mostly", "move", "much", "must", "my", "myself", "name", "namely", "neither", "never", "nevertheless", "next", "nine", "no", "nobody", "none", "noone", "nor", "not", "nothing", "now", "nowhere", "of", "off", "often", "on", "once", "one", "only", "onto", "or", "other", "others", "otherwise", "our", "ours", "ourselves", "out", "over", "own", "part", "per", "perhaps", "please", "put", "rather", "re", "same", "see", "seem", "seemed", "seeming", "seems", "serious", "several", "she", "should", "show", "side", "since", "sincere", "six", "sixty", "so", "some", "somehow", "someone", "something", "sometime", "sometimes", "somewhere", "still", "such", "system", "take", "ten", "than", "that", "the", "their", "them", "themselves", "then", "thence", "there", "thereafter", "thereby", "therefore", "therein", "thereupon", "these", "they", "thickv", "thin", "third", "this", "those", "though", "three", "through", "throughout", "thru", "thus", "to", "together", "too", "top", "toward", "towards", "twelve", "twenty", "two", "un", "under", "until", "up", "upon", "us", "very", "via", "was", "we", "well", "were", "what", "whatever", "when", "whence", "whenever", "where", "whereafter", "whereas", "whereby", "wherein", "whereupon", "wherever", "whether", "which", "while", "whither", "who", "whoever", "whole", "whom", "whose", "why", "will", "with", "within", "without", "would", "yet", "you", "your", "yours", "yourself", "yourselves", "the"];
+	scope.index_method = "Lucene";
+	scope.q = "";
+	scope.corrected_query = "";
+	scope.display_corrected_query = false;
+	scope.filtered_suggestions = [];
 
-    //fetch the csv file here and store
-    http({
-        method: 'GET',
-        url: '/getcsv'
-    }).then(function successCallback(response) {
-            //console.log(response.data);
-            scope.processCSV(response.data);
-            window.file_csv = scope.file_csv;
-        }, function errorCallback(response) {
-            console.log(response)
-        });
+	scope.file_csv = [];
 
-    scope.equalQueries = function () {
-        // if(scope.corrected_query.split("/\s/").length!=scope.q.trim().split("/\s/").length)
-        //     return false;
-        // for(var i=0;i<scope.corrected_query.split("[ ]+").length;i++)
-        //     if(scope.corrected_query.split("[/\s/")[i]!=scope.q.trim().split("/\s/")[i])
-        //         return false;
-        // return true;
-        if(scope.corrected_query===scope.q)
-            return true;
-        return false;
-    }
-    
-    scope.handleSubmit = function () {
-        console.log('handleSubmit')
-        http({
-            method: 'POST',
-            url: '/getcorrectedsentence',
-            params: {
-                sentence: scope.q==undefined ? "*:*" : scope.q
-            }
-        }).then(function successCallback(response) {
-            scope.corrected_query = response.data;
-            //decide if search instead for option
-            console.log(response.data);
+	//fetch the csv file here and store
+	http({
+		method: 'GET',
+		url: '/getcsv'
+	}).then(function successCallback(response) {
+		scope.processCSV(response.data);
+		window.file_csv = scope.file_csv;
+	}, function errorCallback(response) {
+		console.log(response)
+	});
 
-            if(scope.equalQueries()){
-                console.log("they are equal queries")
-                scope.display_corrected_query = false;
-            }
-            else {
-                console.log("they are unequal queries")
-                scope.display_corrected_query = true;
-                if(scope.corrected_query==="*:*")
-                    scope.display_corrected_query = false;
-            }
-            scope.makeQuery(scope.corrected_query);
-        }, function errorCallback(response) {
-            console.log(response)
-        });
-    };
+	scope.equalQueries = function () {
+		if (scope.corrected_query === scope.q)
+			return true;
+		return false;
+	}
 
-    scope.makeQuery = function (query) {
-        http({
-            method: 'GET',
-            url: scope.index_method==="PageRank" ? encodeURI('http://localhost:8983/solr/nbcnews/select?indent=on&q='+query+'&wt=json&sort=pageRankFile desc') : encodeURI('http://localhost:8983/solr/nbcnews/select?indent=on&q='+query+'&wt=json')
-        }).then(function successCallback(response) {
-            console.log(response)
-            console.log(response.data.response.docs.length)
-            scope.docs = response.data.response.docs;
-            window.docs = scope.docs;
-            scope.data = response.data;
-        }, function errorCallback(response) {
-            console.log(response)
-        });
-    }
+	scope.handleSubmit = function () {
+		http({
+			method: 'POST',
+			url: '/getcorrectedsentence',
+			params: {
+				sentence: scope.q == undefined ? "*:*" : scope.q
+			}
+		}).then(function successCallback(response) {
+			scope.corrected_query = response.data;
 
-    scope.processCSV = function(allText) {
-        // split content based on new line
-        var allTextLines = allText.toString().split(/\r\n|\n/);
-        console.log(allTextLines.length)
+			if (scope.equalQueries()) {
+				scope.display_corrected_query = false;
+			} else {
+				scope.display_corrected_query = true;
+				if (scope.corrected_query === "*:*")
+					scope.display_corrected_query = false;
+			}
+			scope.makeQuery(scope.corrected_query);
+		}, function errorCallback(response) {
+			console.log(response)
+		});
+	};
 
-        for (var i = 0; i < allTextLines.length; i++) {
-            // split content based on comma
-            var data = allTextLines[i].split(',');
-            scope.file_csv[data[0]] = data[1];
-        }
-        console.log('csv processing done');
-    };
+	scope.makeQuery = function (query) {
+		http({
+			method: 'GET',
+			url: scope.index_method === "PageRank" ? encodeURI('http://localhost:8983/solr/nbcnews/select?indent=on&q=' + query + '&wt=json&sort=pageRankFile desc') : encodeURI('http://localhost:8983/solr/nbcnews/select?indent=on&q=' + query + '&wt=json')
+		}).then(function successCallback(response) {
+			scope.docs = response.data.response.docs;
+			window.docs = scope.docs;
+			scope.data = response.data;
+		}, function errorCallback(response) {
+			console.log(response)
+		});
+	}
 
-    scope.getFileName = function (file_id) {
-        return scope.file_csv[file_id];
-    }
+	scope.processCSV = function (allText) {
+		// split content based on new line
+		var allTextLines = allText.toString().split(/\r\n|\n/);
 
-    scope.getSuggestions = function (val) {
-        console.log("val = "+val);
-        scope.q = val;
-        var autocorrect_word = scope.q;
-        var prepend = val.trim().split(/[ ]+/);
-        if(prepend.length>1){
-            autocorrect_word = prepend[prepend.length-1];
-        }
-        console.log("autocorrect_word = "+autocorrect_word);
-        return http({
-            method: 'GET',
-            url: "http://localhost:8983/solr/nbcnews/suggest?indent=on&q="+autocorrect_word+"&wt=json"
-        }).then(function successCallback(response) {
-            console.log("call success")
-            console.log(response.data["suggest"]["suggest"][autocorrect_word]["suggestions"]);
-            scope.suggestions = response.data["suggest"]["suggest"][autocorrect_word]["suggestions"];
-            var filtered_suggestions_with_undefined = scope.filterSuggestions(scope.suggestions);
-            var filtered_suggestions = [];
-            var multi_word = false;
-            if(prepend.length>1){
-                prepend.pop();
-                multi_word = true;
-            }
-            console.log("came here");
-            prepend = prepend.join(" ");
-            console.log("prepend = "+prepend);
-            filtered_suggestions_with_undefined.forEach(function (filtered_suggestion) {
-                if(filtered_suggestion!=undefined)
-                    filtered_suggestions.push(multi_word===true ? prepend+" "+filtered_suggestion : filtered_suggestion);
-            })
-            console.log(filtered_suggestions)
-            scope.filtered_suggestions = filtered_suggestions;
-            return filtered_suggestions;
-        }, function errorCallback(response) {
-            console.log("call failed")
-            console.log(response)
-        });
-    }
+		for (var i = 0; i < allTextLines.length; i++) {
+			// split content based on comma
+			var data = allTextLines[i].split(',');
+			scope.file_csv[data[0]] = data[1];
+		}
+	};
 
-    scope.filterSuggestions = function (suggestions) {
-        return suggestions.map(function (word) {
-            if(!(word.term.indexOf(".")>=0 || word.term.indexOf("_")>=0 || word.term.indexOf(":")>=0 || word.term===scope.q || scope.hasStopWord(word.term))){
-                return word.term;
-            }
-        })
-    }
+	scope.getFileName = function (file_id) {
+		return scope.file_csv[file_id];
+	}
 
-    scope.hasStopWord = function (word) {
-        for(var i=0;i<scope.stopwords.length;i++){
-            if(word===scope.stopwords[i])
-                return true;
-        }
-        return false;
-    }
+	scope.getSuggestions = function (val) {
+		scope.q = val;
+		var autocorrect_word = scope.q;
+		var prepend = val.trim().split(/[ ]+/);
+		if (prepend.length > 1) {
+			autocorrect_word = prepend[prepend.length - 1];
+		}
+		return http({
+			method: 'GET',
+			url: "http://localhost:8983/solr/nbcnews/suggest?indent=on&q=" + autocorrect_word + "&wt=json"
+		}).then(function successCallback(response) {
+			scope.suggestions = response.data["suggest"]["suggest"][autocorrect_word]["suggestions"];
+			var filtered_suggestions_with_undefined = scope.filterSuggestions(scope.suggestions);
+			var filtered_suggestions = [];
+			var multi_word = false;
+			if (prepend.length > 1) {
+				prepend.pop();
+				multi_word = true;
+			}
+			prepend = prepend.join(" ");
+			filtered_suggestions_with_undefined.forEach(function (filtered_suggestion) {
+				if (filtered_suggestion != undefined)
+					filtered_suggestions.push(multi_word === true ? prepend + " " + filtered_suggestion : filtered_suggestion);
+			})
+			scope.filtered_suggestions = filtered_suggestions;
+			return filtered_suggestions;
+		}, function errorCallback(response) {
+			console.log(response)
+		});
+	}
 
-    scope.getSnippet = function (url) {
-        //fetch url into text variable
-        return http({
-            method: 'GET',
-            url: '/getsnippet?url='+url
-        }).then(function successCallback(response) {
-            var text = response.data;
+	scope.filterSuggestions = function (suggestions) {
+		return suggestions.map(function (word) {
+			if (!(word.term.indexOf(".") >= 0 || word.term.indexOf("_") >= 0 || word.term.indexOf(":") >= 0 || word.term === scope.q || scope.hasStopWord(word.term))) {
+				return word.term;
+			}
+		})
+	}
 
-            var sentences = text.split("/(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s/");
+	scope.hasStopWord = function (word) {
+		for (var i = 0; i < scope.stopwords.length; i++) {
+			if (word === scope.stopwords[i])
+				return true;
+		}
+		return false;
+	}
 
-            var pattern_array = [];
-            var query_list = scope.corrected_query.split(/\s/);
-            for(var i=0, len = query_list.length; i<len; i++)
-                pattern_array.push(".*?(.*? "+query_list[i]+"[\.?| .*?]).*?");
-            var max_count = 0;
-            var snippet = "", snippet2 = "";
-            for(var i=0;i<sentences.length;i++){
-                var count = 0;
-                for(var j=0;j<pattern_array.length;j++){
-                    if(sentences[i].match(pattern_array[j]))
-                        count++;
-                }
-                if(count>max_count){
-                    max_count = count;
-                    snippet2 = snippet;
-                    snippet = sentences[i];
-                }
-            }
-            return "... "+snippet+" ... "+snippet2+" ...";
-        }, function errorCallback(response) {
-            console.log(response)
-        });
-
-        //console.log(url)
-        // htmlToText.fromFile(path.join(__dirname, 'tikaoutput/'+url+".txt"), {
-        //     tables: ['#invoice', '.address']
-        // }, (err, text) => {
-        //     if (err) return console.error(err);
-        //     console.log(text);
-        // });
-        //return "text";
-    }
+	scope.getSnippet = function (url) {
+		//fetch url into text variable
+		return http({
+			method: 'GET',
+			url: '/getsnippet?url=' + url
+		}).then(function successCallback(response) {
+			var text = response.data;
+			var sentences = text.split("/(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s/");
+			var pattern_array = [];
+			var query_list = scope.corrected_query.split(/\s/);
+			for (var i = 0, len = query_list.length; i < len; i++)
+				pattern_array.push(".*?(.*? " + query_list[i] + "[\.?| .*?]).*?");
+			var max_count = 0;
+			var snippet = "",
+				snippet2 = "";
+			for (var i = 0; i < sentences.length; i++) {
+				var count = 0;
+				for (var j = 0; j < pattern_array.length; j++) {
+					if (sentences[i].match(pattern_array[j]))
+						count++;
+				}
+				if (count > max_count) {
+					max_count = count;
+					snippet2 = snippet;
+					snippet = sentences[i];
+				}
+			}
+			return "... " + snippet + " ... " + snippet2 + " ...";
+		}, function errorCallback(response) {
+			console.log(response)
+		});
+	}
 
 }]);
